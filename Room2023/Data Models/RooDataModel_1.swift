30ed783aeec083c1552d3f944761efa30926812e
Owen Hildreth <ohildreth@mines.edu>
2023-01-30 15:05:02 -0700

    Some Small comment change

--- Room2023/Data Models/RooDataModel_1.swift
+++ Room2023/Data Models/RooDataModel_1.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class RoomDataModel_1: ObservableObject {
+    // some change
     @Published var numberOfStudents = 0
     @Published var numberOfTeachers = 0  {
         didSet {
